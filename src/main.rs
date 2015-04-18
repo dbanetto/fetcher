78054a4fdb4492c1b77a61a6aa49eb680f38fe45
David Barnett <david@barnett.net.nz>
2015-04-19 11:23:14 +1200

    Settings file
    + Settings to fetchd
    + fetch_url settings option
    * update hyper to 0.3.13
    * update rust to 1.0.0-beta.2

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,31 @@ extern crate rustc_serialize;
 extern crate fetcher;
 
 use std::io::Read;
+use std::fs::File;
 
-
+use rustc_serialize::json;
 use rustc_serialize::json::Json;
 
 use fetcher::client::Client;
 
+#[derive(RustcDecodable)]
+struct Settings {
+    fetch_url: String,
+}
+
+fn load_settings() -> Settings {
+    let mut settings_file = File::open("settings.json").unwrap();
+    let mut settings_content = String::new();
+    settings_file.read_to_string(&mut settings_content).unwrap();
+
+    json::decode::<Settings>(&settings_content).unwrap()
+}
+
 fn main() {
+    let settings = load_settings();
+
     // Create a client to fetch Web UI
-    let mut client = Client::new("http://127.0.0.1/").unwrap();
+    let mut client = Client::new(&settings.fetch_url).unwrap();
 
     // get '/series/'
     let mut res = client.get("series").unwrap();
